9c722e3220e66be53766541f00ec3d267094fb2c
noman <nomankdevice@gmail.com>
2025-09-06 00:54:05 +0500

    Solution for ContainsDuplicate Problem

---
+++ .history/arrays/ContainsDuplicates_217_20250906004553.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dic={}
+        for num in nums:
+            if num not in dic:
+                dic[num]=False
+            else: #if num appears twice
+                return True
+        return False
