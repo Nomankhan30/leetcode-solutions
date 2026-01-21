738145368d0a68e5f69f03b2238bed9d60b9a175
noman <nomankdevice@gmail.com>
2026-01-21 23:58:01 +0500

    Added Solution for Problem 169

---
+++ arrays_hashing/majority_element_169.ts
@@ -0,0 +1,14 @@
+function majorityElement(nums: number[]): number {
+    let count = 0;
+    let candidate: number | null = null;
+
+    for (const num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+
+    //candidate will be the majority element
+    return candidate!; //will not be null
+}
